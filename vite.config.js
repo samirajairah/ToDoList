390513b3300eb45da4359a7af2f26465d7f22fe4
JaiMngndto <samirajairah@gmail.com>
2024-02-16 14:54:32 +0800

    push 3

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["react-icons/bs"],
+    },
+  },
   plugins: [react()],
 });
